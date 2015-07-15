ead29bd705196a254a8d3f3f5be967ce66f15f12
Andrew <Asaganda@gmail.com>
2015-07-15 14:38:49 -0400

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,35 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	# editing a post
+	def edit
+		# this finds the post that we want to edit
+		@post = Post.find(params[:id])
+	end
+
+	# updates the post that we are trying to edit
+	def update
+		# this finds the post that we want to edit
+		@post = Post.find(params[:id])
+		# we have the permit for title and body because we included them in the private method below 
+		if @post.update(params[:post].permit(:title, :body))
+			# this redirects to the show page
+			redirect_to @post
+		else
+			# edit form to show
+			render 'edit'
+		end
+	end
+	# method that deletes a post
+	def destroy
+		# this finds the post that we want to delete
+		@post = Post.find(params[:id])
+		# deletes the post
+		@post.destroy
+		# redirects to all the root where list of posts are shown
+		redirect_to root_path
+	end
+
 	# security feature
 	# WARNING this must be at the end of all methods
 	# or else any methods below will be private/errors will occur
